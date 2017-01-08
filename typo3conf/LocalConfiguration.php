aad5518bac2dc31490f74cfb82c1ca14875e40d2
Alexander Gunkel <alexandergunkel@gmail.com>
2017-01-08 23:36:52 +0100

    Add template extension

---
+++ typo3conf/LocalConfiguration.php
@@ -0,0 +1,67 @@
+<?php
+return [
+    'BE' => [
+        'debug' => true,
+        'explicitADmode' => 'explicitAllow',
+        'installToolPassword' => '$P$CVzYMChbkxr3e9Qypx8NXeA6bcdnKA1',
+        'loginSecurityLevel' => 'rsa',
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8',
+                'dbname' => 'typo3',
+                'driver' => 'mysqli',
+                'host' => '172.17.0.1',
+                'password' => 'admin',
+                'port' => 3306,
+                'user' => 'root',
+            ],
+        ],
+    ],
+    'EXT' => [
+        'extConf' => [
+            'rsaauth' => 'a:1:{s:18:"temporaryDirectory";s:0:"";}',
+            'saltedpasswords' => 'a:2:{s:3:"BE.";a:4:{s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}s:3:"FE.";a:5:{s:7:"enabled";i:1;s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}}',
+            'typo3template' => 'a:0:{}',
+        ],
+    ],
+    'FE' => [
+        'cHashIncludePageId' => true,
+        'debug' => true,
+        'loginSecurityLevel' => 'rsa',
+    ],
+    'GFX' => [
+        'jpg_quality' => '80',
+    ],
+    'MAIL' => [
+        'transport_sendmail_command' => ' -t -i ',
+    ],
+    'SYS' => [
+        'caching' => [
+            'cacheConfigurations' => [
+                'extbase_object' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
+                    'groups' => [
+                        'system',
+                    ],
+                    'options' => [
+                        'defaultLifetime' => 0,
+                    ],
+                ],
+            ],
+        ],
+        'devIPmask' => '*',
+        'displayErrors' => 1,
+        'enableDeprecationLog' => 'file',
+        'encryptionKey' => '877b702ddf0f69302cc1f7f931d91d3be691949f94de2f96ec9193063e4b7f9bbbaaf1a3d51df4fd475e3af88a4e1ffe',
+        'exceptionalErrors' => 28674,
+        'isInitialDatabaseImportDone' => true,
+        'isInitialInstallationInProgress' => false,
+        'sitename' => 'New TYPO3 site',
+        'sqlDebug' => 1,
+        'systemLogLevel' => 0,
+        'trustedHostsPattern' => '.*',
+    ],
+];
